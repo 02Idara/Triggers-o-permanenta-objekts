f1f1009c245c4bd9881ec6485facf936ca5a0043
Axevalla <ssas25chza@axevalla.fhsk.se>
2025-12-11 11:58:11 +0100

    klar med laborint

--- Assets/spelare.cs
+++ Assets/spelare.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class spelare : MonoBehaviour
 {
+    [SerializeField] private float playerSpeed = 3;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.position = new Vector3(0, -3.5f, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * playerSpeed *  Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * playerSpeed * Time.deltaTime);
+        } 
     }
 }
